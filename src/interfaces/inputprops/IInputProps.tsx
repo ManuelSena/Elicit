2b09121b908491820c9a6d9c817a269cf2cc23ab
ManuelS <manuelsena@me.com>
2020-03-21 20:41:52 -0700

    sd

--- src/interfaces/inputprops/IInputProps.tsx
+++
@@ -1,8 +0,0 @@
-﻿export interface IInputProps {
-    name: string;
-    label: string;
-    placeholder?: string;
-    value: string;
-    onChange: (fieldName: string, value: string) => void;
-    error?: string;
-}
